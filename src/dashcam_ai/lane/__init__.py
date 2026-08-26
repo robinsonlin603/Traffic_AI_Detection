72486b65f78faafb75e959b0a2e871f41ecc69c3
robinsonlin603 <93252094+robinsonlin603@users.noreply.github.com>
2026-08-26 17:03:04 +0800

    Revert "Feature/milestone2 slice3"

--- src/dashcam_ai/lane/__init__.py
+++
@@ -1,7 +0,0 @@
-"""公開車道偵測與車道歸屬分析介面。"""
-
-from dashcam_ai.lane.base import LaneDetector
-from dashcam_ai.lane.configured import ConfiguredLaneDetector
-from dashcam_ai.lane.membership import LaneMembershipEvaluator
-
-__all__ = ["ConfiguredLaneDetector", "LaneDetector", "LaneMembershipEvaluator"]
--- src/dashcam_ai/motion/__init__.py
+++
@@ -1,6 +0,0 @@
-"""公開自車相機運動估算介面與 OpenCV 實作。"""
-
-from dashcam_ai.motion.base import EgoMotionEstimator
-from dashcam_ai.motion.opencv import OpenCVEgoMotionEstimator
-
-__all__ = ["EgoMotionEstimator", "OpenCVEgoMotionEstimator"]
